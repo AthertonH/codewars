f9680fb549ba695f615d2917a34eeb82abd6887d
Harrison Atherton <athertonharrison@gmail.com>
2022-08-25 18:19:01 -0700

    codewars

--- 6_compare_powers.py
+++ 6_compare_powers.py
@@ -12,26 +12,30 @@
 # compare_powers([2,10],[2,10])==0
 # compare_powers([3,9],[5,6])==-1
 # compare_powers([7,7],[5,8])==-1
-# Only positive integers will be tested, incluing bigger numbers - you are warned now, so be diligent try to implement
+# Only positive integers will be tested, including bigger numbers - you are warned now, so be diligent try to implement
 # an efficient solution not to drain too much on CW resources ;)!
 
-# def compare_powers(n1, n2):
-#     if n1[0] > 0 and n1[1] > 0 and n2[0] > 0 and n2[1] > 0:
-#         number1 = (int(n1[0]) ** int(n1[1]))
-#         number2 = (int(n2[0]) ** int(n2[1]))
-#         if number1 > number2:
-#             return -1
-#         elif number1 < number2:
-#             return 1
-#         else:
-#             return 0
+import math
 
+# def compare_powers(n1, n2):
+#     number1 = pow(n1[0], n1[1])
+#     number2 = pow(n2[0], n2[1])
+#
+#     if number1 > number2:
+#         return -1
+#     elif number1 < number2:
+#         return 1
+#     else:
+#         return 0
+
+import math
 def compare_powers(n1, n2):
-        number1 = pow(n1[0], n1[1])
-        number2 = pow(n2[0], n2[1])
-        if number1 > number2:
-            return -1
-        elif number1 < number2:
-            return 1
-        else:
-            return 0
+    number1 = n1[1] * math.log(n1[0])
+    number2 = n2[1] * math.log(n2[0])
+
+    if number1 > number2:
+        return -1
+    elif number1 < number2:
+        return 1
+    else:
+        return 0
